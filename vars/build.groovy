b3ec9dbd87d7b0dd377bfb9b827e90938a959bda
Iyanu Emmanuel <117942488+Orisuniyanu@users.noreply.github.com>
2025-05-12 18:44:45 +0100

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,8 +1,10 @@
-// inside vars/build.groovy in Jenkins shared lib
 def call(String imageName) {
-    // Copy Dockerfile into current workspace
-    sh 'cp $JENKINS_HOME/workspace/@libs/Jenkins-Library/resource/common/Dockerfile ./Dockerfile'
+    def tag = "${imageName}:${env.BUILD_NUMBER}"
+    echo "Building Docker image: ${tag}"
 
-    // Build Docker image
-    sh "docker build -t ${imageName}:${env.BUILD_NUMBER} -f Dockerfile ."
+    sh """
+        docker build -t ${tag} -f resource/common/Dockerfile .
+    """
+
+    echo "Docker image built successfully: ${tag}"
 }
