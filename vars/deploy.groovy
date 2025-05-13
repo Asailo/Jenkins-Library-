77fd698252cc831700c764e4bf40e06bd4bbe6af
Iyanu Emmanuel <117942488+Orisuniyanu@users.noreply.github.com>
2025-05-13 13:45:36 +0100

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -5,18 +5,19 @@ def call(String imageName) {
     withCredentials([
         string(credentialsId: 'vm-host', variable: 'Remote_Host'),
         string(credentialsId: 'vm-user', variable: 'Remote_User')
-    ])    
-    echo "Deploying Docker image: ${tag} to VM"
+    ]) {    
+            echo "Deploying Docker image: ${tag} to VM"
 
-    sshagent (credentials: ['ssh-vm-creds-id']) {
-        sh """
-            ssh -o StrictHostKeyChecking=no ${Remote_User}@${Remote_Host} '
-                docker pull ${tag} &&
-                docker-compose -f resource/common/docker-compose.yml down || true &&
-                docker-compose -f resource/common/docker-compose.yml up -d
-            '
-        """
-    }
+            sshagent (credentials: ['ssh-vm-creds-id']) {
+                sh """
+                    ssh -o StrictHostKeyChecking=no ${Remote_User}@${Remote_Host} '
+                        docker pull ${tag} &&
+                        docker-compose -f resource/common/docker-compose.yml down || true &&
+                        docker-compose -f resource/common/docker-compose.yml up -d
+                    '
+                """
+            }
+        }   
 
     echo "Docker image deployed successfully to the VM: ${tag}"
 }
