fae61a810d48292b689d7df75ff51a620df940f8
Iyanu Emmanuel <117942488+Orisuniyanu@users.noreply.github.com>
2025-05-13 10:15:53 +0100

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -2,12 +2,11 @@ def call(String imageName) {
     def tag = "${imageName}:${env.BUILD_NUMBER}"
 
     withCredentials([usernamePassword(credentialsId: 'dockerhub-creds-id', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
-        sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin"
-        sh "docker push ${imageName}:${env.BUILD_NUMBER}"
+        sh """
+            echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin
+            docker push ${tag}
+        """
     }
 
-
     echo "Image pushed: ${tag}"
-
 }
-
