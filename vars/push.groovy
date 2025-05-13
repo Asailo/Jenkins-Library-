0330ed1381e3126643e402c5cdda9ba09a8aaf29
Iyanu Emmanuel <117942488+Orisuniyanu@users.noreply.github.com>
2025-05-13 12:10:33 +0100

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -3,7 +3,7 @@ def call(String imageName) {
 
     echo "Preparing to push Docker image: ${tag}"
 
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-creds-id', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-jenkins', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh """
             set -ex
             echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin
