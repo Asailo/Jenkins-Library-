af92d41cecd61a62c3a64d3cf8c33f01b3ce1b65
Iyanu Emmanuel <117942488+Orisuniyanu@users.noreply.github.com>
2025-05-13 11:45:16 +0100

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -2,9 +2,13 @@ def call(String imageName) {
     def tag = "${imageName}:${env.TAG}"
     echo "Building Docker image: ${tag}"
 
+    // Build the image
     sh """
         docker build -t ${tag} .
     """
+
+    // Verify if the image exists locally
+    sh "docker images | grep ${tag}"
 
     echo "Docker image built successfully: ${tag}"
 }
