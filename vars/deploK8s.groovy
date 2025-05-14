d54863c72731d2e0a2c3dec940ff31870b11e6e2
Iyanu Emmanuel <117942488+Orisuniyanu@users.noreply.github.com>
2025-05-14 16:25:19 +0100

    Update deploK8s.groovy

--- vars/deploK8s.groovy
+++ vars/deploK8s.groovy
@@ -1,9 +1,14 @@
 def call(String imageName) {
     withCredentials([file(credentialsId: 'kubeconfig-prod', variable: 'KUBECONFIG_FILE')]) {
         sh """
-            echo '[INFO] Copying kubeconfig to workspace'
-            cp \$KUBECONFIG_FILE \$WORKSPACE/kubeconfig-prod.yaml
-            export KUBECONFIG=\$WORKSPACE/kubeconfig-prod.yaml
+            echo '[INFO] Creating temp directory for kubeconfig'
+            mkdir -p \$WORKSPACE/tmp
+
+            echo '[INFO] Copying kubeconfig to temp directory'
+            cp \$KUBECONFIG_FILE \$WORKSPACE/tmp/kubeconfig-prod.yaml
+
+            export KUBECONFIG=\$WORKSPACE/tmp/kubeconfig-prod.yaml
+
             echo '[INFO] Applying Kubernetes deployment and service files'
             kubectl apply -f k8s/deployment.yaml
             kubectl apply -f k8s/service.yaml
