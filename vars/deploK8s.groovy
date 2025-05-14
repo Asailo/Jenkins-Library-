64346ee57b0997f4618440feef1eb7e95281c4f6
Iyanu Emmanuel <117942488+Orisuniyanu@users.noreply.github.com>
2025-05-14 11:36:27 +0100

    Update deploK8s.groovy

--- vars/deploK8s.groovy
+++ vars/deploK8s.groovy
@@ -2,8 +2,8 @@ def call(String imageName) {
     withCredentials([file(credentialsId: 'kubeconfig-prod', variable: 'KUBECONFIG_FILE')]) {
         sh """
             echo '[INFO] Copying kubeconfig to workspace'
-            cp \$KUBECONFIG_FILE ${kubeconfig-prodPath}
-            export KUBECONFIG=${kubeconfig-prodPath}
+            cp \$KUBECONFIG_FILE ./kubeconfig-prod.yaml
+            export KUBECONFIG=./kubeconfig-prod.yaml
             echo '[INFO] Applying Kubernetes deployment and service files'
             kubectl apply -f ${k8sDir}/deployment.yaml
             kubectl apply -f ${k8sDir}/service.yaml
